5b5b414fca54078610e4e75a9e12df9e8d8c23ed
Antti Plathan <plathan@y-c02cg1mymd6r.local>
2021-02-06 20:22:58 +0200

    Sivupyyntöjen parsinta toimii ja näyttää oikean kuvan

--- app.js
+++ app.js
@@ -71,38 +71,69 @@ app.command('/ttv', async ({ command, ack, say, client, body }) => {
   // Parsitaan käyttäjän antamasta komennosta vain ensimmäinen parametri
   const requestedPage = (command.text).split(' ')[0];
   console.log(requestedPage);
-  var ttvPage = 100;
+  // ttvPage pysyy falsena ellei pyynnöst löydy validia sivupyyntöä
+  var ttvPage = false;
 
-  // Aloitetaan tutkimalla, onko komennon jälkeen annettu ensimmäinen parametri sana
-  if (isNaN(ttvPage)){
-    switch (ttvPage) {
-      // Vastataan help-komentoon
-      case 'help':
-        await say(
-          {
-            "response_type": "ephemeral",
-            "text": "Näin käytät /ttv -komentoa:",
-            "attachments":[
-                {
-                    "text":"Anna sivun numero `/ttv 100` tai osaston nimi `/ttv kotimaa` avataksesi Teksti-TV:n.\nOsastojen nimet ovat `etusivu`, `hakemistot`, `kotimaa`, `ulkomaat`, `talous`, `sää`, `liikenne`, `urheilu`, `nhl`, `eurojalkapallo`, `veikkaus`, `tv-ohjelmat`, `ohjelmaopas`, `alueuutiset`, `news`, `svenska`, `viikkomakasiini`.\n"
-                }
-            ]
-          }
-        );
-        break;
-      default:
-        await say('Väärä komento');
-    };
+  // Löytyykö pyydetty sivu dictionarystä? Samalla se tarkoittaa myös sitä, että pyyntö ei ollut numero
+  if (requestedPage in pageDict) {
+    console.log("Sivu löytyy dictionarystä! Asetetaan näytettäväksi TTV-sivuksi:");
+    // Haetaan dictionarystä pyydettyä osastoa vastaava ttv-sivunumero
+    ttvPage = pageDict[requestedPage]; 
+    console.log(ttvPage);
+  } else if (isNaN(requestedPage)) {
+      // Jos pyyntö ei ole numero (ei ole suoraa testiä sille, että ON numero), ttvPage pysyy falsena ja käyttäjälle tullaan näyttämään helppi
+      console.log("Pyydetty sivu ei ole numero eikä kuulu tunnettujen osastojen joukkoon:");
+      console.log(requestedPage);
+      ttvPage = false; // Redundantti, mutta luettavuuden helpottamiseksi
+    } else {
+        // Nyt tiedetään, että pyyntö oli numero. Muutetaan se kokonaisluvuksi
+        const pageNumber = parseInt(requestedPage, 10);
+
+        // Ylen TTV:n sallitut sivunumerot ovat 100-899
+        if (pageNumber > 99 && pageNumber < 900) {
+          console.log("Sivunumero on sallituissa rajoissa.");
+          ttvPage = pageNumber; // Asetetaan haettava TTV-sivunumero
+        } else {
+          console.log("Sivunumero ei ole sallituissa rajoissa!");
+          ttvPage = false; // Redundantti, mutta luettavuuden helpottamiseksi
+        }
+      }
+     
+  
+
+  
+  if (ttvPage) {
+    // Näytettävä sivu tiedetään, yritetään näyttää se
+    console.log("Perillä ollaan, yritetään näyttää pyydetty sivunumero: ");
+    console.log(ttvPage);
+
+    await say({
+      text: `https://external.api.yle.fi/v1/teletext/images/${ttvPage}/1.png?${process.env.TTV_API_KEY}`,
+      replace_original: false,
+      response_type: `ephemeral`,
+      unfurl_links: true,
+      unfurl_media: true
+    });
   } else {
-      const pageNumber = parseInt(requestedPage, 10);
-      await say({
-        text: `https://external.api.yle.fi/v1/teletext/images/${pageNumber}/1.png?${process.env.TTV_API_KEY}`,
-        replace_original: false,
-        response_type: `ephemeral`,
-        unfurl_links: true,
-        unfurl_media: true
-      });
-    }
+    // Kelvollista TTV-sivua ei voitu asettaa, näytetään helppi käyttäjälle
+    console.log("Loppuun päädyttiin, mutta käyttäjän syötteestä ei tunnistettu oikeaa sivunumeroa tai osastoa, tai pyyntö oli help:")
+    console.log(ttvPage);
+    
+    await say(
+      {
+        "response_type": "ephemeral",
+        "text": "Näin käytät /ttv -komentoa:",
+        "attachments":[
+            {
+                "text":"Anna haettavan TTV-sivun numero `/ttv 100` tai osaston nimi `/ttv kotimaa`.\nOsastojen nimet ovat `etusivu`, `hakemistot`, `kotimaa`, `ulkomaat`, `talous`, `sää`, `liikenne`, `urheilu`, `nhl`, `eurojalkapallo`, `veikkaus`, `tv-ohjelmat`, `ohjelmaopas`, `alueuutiset`, `news`, `svenska`, `viikkomakasiini`.\n"
+            }
+        ]
+      }
+    );
+    
+  }
+
+  
 
 
 });  
